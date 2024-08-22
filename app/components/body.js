21cb408f11f079cf66f384079ea9074d20e775fd
synakhanna <sk9921@nyu.edu>
2024-08-22 01:10:42 -0700

    more changes

--- app/components/body.js
+++ app/components/body.js
@@ -1,48 +1,60 @@
 import { Box, Button, Stack, TextField } from '@mui/material';
 import styles from './body.module.css';
+import { useState } from 'react';
 
-export default function Body({ messages, sendMessage, message, setMessage }) {
+export default function Body() {
+  const [messages, setMessages] = useState([
+    {
+      role: 'assistant',
+      content: `Welcome to CraveQuest. How can I help with your food cravings today?`,
+    },
+  ]);
+  const [message, setMessage] = useState('');
+
+  const sendMessage = async () => {
+    setMessage('');
+    setMessages((messages) => [
+      ...messages,
+      { role: 'user', content: message },
+      { role: 'assistant', content: '' },
+    ]);
+
+    const response = fetch('/api/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify([...messages, { role: 'user', content: message }]),
+    }).then(async (res) => {
+      const reader = res.body.getReader();
+      const decoder = new TextDecoder();
+      let result = '';
+
+      return reader.read().then(function processText({ done, value }) {
+        if (done) {
+          return result;
+        }
+        const text = decoder.decode(value || new Uint8Array(), { stream: true });
+        setMessages((messages) => {
+          let lastMessage = messages[messages.length - 1];
+          let otherMessages = messages.slice(0, messages.length - 1);
+          return [
+            ...otherMessages,
+            { ...lastMessage, content: lastMessage.content + text },
+          ];
+        });
+        return reader.read().then(processText);
+      });
+    });
+  };
+
   return (
-    <Box
-      width="100vw"
-      height="100vh"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      className={styles.body}
-    >
-      <Stack
-        direction={'column'}
-        width="500px"
-        height="600px"
-        border="1px solid #ffa240"
-        p={2}
-        spacing={3}
-        className={styles.chatBox}
-      >
-        <Stack
-          direction={'column'}
-          spacing={2}
-          flexGrow={1}
-          overflow="auto"
-          maxHeight="100%"
-        >
+    <Box className={styles.body}>
+      <Stack spacing={3} className={styles.chatBox}>
+        <Stack spacing={2} flexGrow={1} overflow="auto">
           {messages.map((message, index) => (
-            <Box
-              key={index}
-              display="flex"
-              justifyContent={
-                message.role === 'assistant' ? 'flex-start' : 'flex-end'
-              }
-            >
-              <Box
-                className={
-                  message.role === 'assistant'
-                    ? styles.assistantMessage
-                    : styles.userMessage
-                }
-              >
+            <Box key={index} display="flex" justifyContent={ message.role === 'assistant' ? 'flex-start' : 'flex-end'}>
+              <Box className={ message.role === 'assistant' ? styles.assistantMessage : styles.userMessage } >
                 {message.content}
               </Box>
             </Box>
@@ -56,19 +68,18 @@ export default function Body({ messages, sendMessage, message, setMessage }) {
             value={message}
             onChange={(e) => setMessage(e.target.value)}
             InputProps={{
-              style: { backgroundColor: '#ffa240', color: '#2c1608' },
+              style: { backgroundColor: '#b18569', color: '#281705' },
             }}
             InputLabelProps={{
-              style: { color: '#2c1608' },
+              style: { color: '#281705' , fontFamily: 'JejuMyeongjo'},
+            }}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                sendMessage(e);
+              }
             }}
           />
-          <Button
-            variant="contained"
-            style={{ backgroundColor: '#ffa240', color: '#2c1608' }}
-            onClick={sendMessage}
-          >
-            send
-          </Button>
+          <Button onClick={sendMessage} className={styles.sendButton}>Send</Button>
         </Stack>
       </Stack>
     </Box>
--- app/page.js
+++ app/page.js
@@ -1,58 +1,18 @@
 'use client'
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import Body from './components/body';
 import Footer from './components/footer';
 import Header from './components/header';
+import smoothScrollIntoView from 'smooth-scroll-into-view-if-needed';
+
 
 export default function Home() {
-  const [messages, setMessages] = useState([
-    {
-      role: 'assistant',
-      content: `Welcome to CraveQuest. How can I help with your food cravings today?`,
-    },
-  ]);
-  const [message, setMessage] = useState('');
-
   const chatSectionRef = useRef(null);
 
   const handleScrollToChat = () => {
-    chatSectionRef.current.scrollIntoView({ behavior: 'smooth' });
-  };
-
-  const sendMessage = async () => {
-    setMessage('');
-    setMessages((messages) => [
-      ...messages,
-      { role: 'user', content: message },
-      { role: 'assistant', content: '' },
-    ]);
-
-    const response = fetch('/api/chat', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify([...messages, { role: 'user', content: message }]),
-    }).then(async (res) => {
-      const reader = res.body.getReader();
-      const decoder = new TextDecoder();
-      let result = '';
-
-      return reader.read().then(function processText({ done, value }) {
-        if (done) {
-          return result;
-        }
-        const text = decoder.decode(value || new Uint8Array(), { stream: true });
-        setMessages((messages) => {
-          let lastMessage = messages[messages.length - 1];
-          let otherMessages = messages.slice(0, messages.length - 1);
-          return [
-            ...otherMessages,
-            { ...lastMessage, content: lastMessage.content + text },
-          ];
-        });
-        return reader.read().then(processText);
-      });
+    smoothScrollIntoView(chatSectionRef.current, {
+      behavior: 'smooth',
+      duration: 2000
     });
   };
 
@@ -60,14 +20,9 @@ export default function Home() {
     <div>
       <Header handleScrollToChat={handleScrollToChat} />
       <section ref={chatSectionRef}>
-        <Body
-          messages={messages}
-          sendMessage={sendMessage}
-          message={message}
-          setMessage={setMessage}
-        />
-      </section>
+        <Body />
       <Footer />
+      </section>
     </div>
   );
 }
